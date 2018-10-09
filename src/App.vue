e4e4b88b0bcade028962d776b2066be8c9848450
alexyap1205 <alexyap1205@live.com>
2018-10-09 20:39:02 +1100

    Custom directive

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Built-in directive</h1>
+                <p v-text="'Testing v-text'"></p>
+                <hr>
+                <h1>Custom directive</h1>
+                <p v-highlight:background.delayed="'red'">Highlighted</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style>
+</style>
